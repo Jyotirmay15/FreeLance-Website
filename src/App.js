3e23d502c40efa8ea8d102c66046849d37bab3c8
Jyotirmay <jyomishra99@gmail.com>
2022-09-11 23:44:57 +0530

    First commit MUI React website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "@mui/material/Button";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Button>Test</Button>
           Learn React
         </a>
       </header>
